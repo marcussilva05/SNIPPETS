c947f8105fe7cb79aca3e34f582e862fd027df56
Marcus-Vinicius-Dev <marcus.vini.dev@gmail.com>
2026-03-31 11:53:55 -0300

    edicoes finais do dia 31_03_26

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -11,49 +11,54 @@
 # no PS digite . $PROFILE 
 # teste digitando z+space+tab
 # aparecerá menu de "snippets"
+# 
 
 function z {
     param(
         [string]$comando
     )
 
-    switch ($comando) {
+    switch ($comando) {		
 
-        "bateria" { Get-WmiObject Win32_Battery | Format-Table Caption, EstimatedChargeRemaining }
-        "bluetooth" { Get-WmiObject Win32_PnPEntity | Where-Object {$_.PNPClass -eq "Bluetooth"} | Format-Table Caption }
-        "bios" { Get-WmiObject Win32_BIOS | Select-Object -ExpandProperty SMBIOSBIOSVersion }
-        "buscar_por_nome" { Get-ChildItem -Filter "*documen*" -File -Recurse }
-        "cpu" { Get-WmiObject Win32_Processor | Format-Table Name, MaxClockSpeed, NumberOfCores -AutoSize }
-        "desktop" { Set-Location ~/Desktop }
-	"discoinfo" { Get-WmiObject Win32_DiskDrive | Format-Table DeviceID, Caption, @{Label="Size(GB)";Expression={[math]::round($_.Size/1GB,2)}}, MediaType }
-        "discos" { Get-PSDrive -PSProvider FileSystem | Select-Object -ExpandProperty Name }
-        "disco_espaco" { Get-PSDrive -PSProvider FileSystem | Format-Table Name, @{Label="Used(GB)";Expression={[math]::round($_.Used/1GB,2)}}, @{Label="Free(GB)";Expression={[math]::round($_.Free/1GB,2)}} }
-        "gpu" { Get-WmiObject Win32_VideoController | Format-Table Caption, @{Label="RAM(MB)";Expression={[math]::round($_.AdapterRAM/1MB,2)}} }
-        "home" { Set-Location ~ }
-        "impressoras" { Get-WmiObject Win32_Printer | Format-Table Name }
-        "ip" { ipconfig }
-        "ip_geral" { ipconfig /all }
-        "listar" { Get-ChildItem }
-        "listar_force" { Get-ChildItem -Force }
-        "listar_recursivo" { Get-ChildItem -Recurse }
-        "listar_so_pastas" { Get-ChildItem -Directory }
-        "listar_so_arquivos" { Get-ChildItem -File }
-        "memoria_slots" { Get-WmiObject Win32_PhysicalMemory | Select-Object -ExpandProperty DeviceLocator }
-        "memorias" { Get-WmiObject Win32_PhysicalMemory | Format-Table Capacity, Speed, FormFactor }
-        "modelo_pc" { Get-WmiObject Win32_ComputerSystem | Select-Object -ExpandProperty Model }
-        "onde_estou?" { pwd }
-        "placa_mae" { Get-WmiObject Win32_BaseBoard | Format-Table Manufacturer, Product, Version, SerialNumber -AutoSize }
-        "particoes" { Get-WmiObject Win32_DiskPartition | Select-Object -ExpandProperty DeviceID }
-        "programas_instalados" { Get-WmiObject Win32_Product | Format-Table Name, Version }
-        "rede" { Get-WmiObject Win32_NetworkAdapter | Where-Object {$_.MACAddress} | Format-Table Name, MACAddress }
-        "sistema_operacional" { Get-WmiObject Win32_OperatingSystem | Format-Table Caption, Version, OSArchitecture }
-        "snippet_abrir" { code $PROFILE }
-        "snippet_atualizar" { . $PROFILE }
-		"som" { Get-WmiObject Win32_SoundDevice | Format-Table Caption }
-        "usb" { Get-WmiObject Win32_PnPSignedDriver | Format-Table DeviceName }
-        "usuarios" { Get-LocalUser | Format-Table Name, Enabled }
-        "volta_1_diretorio" { Set-Location .. }
-        "volta_2_diretorios" { Set-Location ..\.. }
+		"nav_buscar_por_nome" { Get-ChildItem -Filter "*documen*" -File -Recurse }
+		"nav_desktop" { Set-Location ~/Desktop }
+		"nav_downloads" { Set-Location ~/Downloads }		
+		"nav_home" { Set-Location ~ }
+        "nav_listar" { Get-ChildItem }
+        "nav_listar_ocultos" { Get-ChildItem -Force }
+        "nav_listar_recursivo" { Get-ChildItem -Recurse }
+        "nav_listar_so_pastas" { Get-ChildItem -Directory }
+        "nav_listar_so_arquivos" { Get-ChildItem -File }
+        "nav_onde_estou?" { pwd }
+        "nav_volta_1_diretorio" { Set-Location .. }
+        "nav_volta_2_diretorios" { Set-Location ..\.. }
+        "nav_volta_desktop" { Set-Location ~/Desktop }
+		"nav_volta_home" { Set-Location ~ }
+        "sys_bateria" { Get-WmiObject Win32_Battery | Format-Table Caption, EstimatedChargeRemaining }
+        "sys_bluetooth" { Get-WmiObject Win32_PnPEntity | Where-Object {$_.PNPClass -eq "Bluetooth"} | Format-Table Caption }
+        "sys_bios" { Get-WmiObject Win32_BIOS | Select-Object -ExpandProperty SMBIOSBIOSVersion }
+        "sys_cpu" { Get-WmiObject Win32_Processor | Format-Table Name, MaxClockSpeed, NumberOfCores -AutoSize }
+		"sys_discoinfo" { Get-WmiObject Win32_DiskDrive | Format-Table DeviceID, Caption, @{Label="Size(GB)";Expression={[math]::round($_.Size/1GB,2)}}, MediaType }
+        "sys_discos" { Get-PSDrive -PSProvider FileSystem | Select-Object -ExpandProperty Name }
+        "sys_disco_espaco" { Get-PSDrive -PSProvider FileSystem | Format-Table Name, @{Label="Used(GB)";Expression={[math]::round($_.Used/1GB,2)}}, @{Label="Free(GB)";Expression={[math]::round($_.Free/1GB,2)}} }
+        "sys_gpu" { Get-WmiObject Win32_VideoController | Format-Table Caption, @{Label="RAM(MB)";Expression={[math]::round($_.AdapterRAM/1MB,2)}} }
+        "sys_impressoras" { Get-WmiObject Win32_Printer | Format-Table Name }
+        "sys_ip" { ipconfig }
+        "sys_ip_geral" { ipconfig /all }
+        "sys_memoria_slots" { Get-WmiObject Win32_PhysicalMemory | Select-Object -ExpandProperty DeviceLocator }
+        "sys_memorias" { Get-WmiObject Win32_PhysicalMemory | Format-Table Capacity, Speed, FormFactor }
+        "sys_modelo_pc" { Get-WmiObject Win32_ComputerSystem | Select-Object -ExpandProperty Model }
+        "sys_placa_mae" { Get-WmiObject Win32_BaseBoard | Format-Table Manufacturer, Product, Version, SerialNumber -AutoSize }
+        "sys_particoes" { Get-WmiObject Win32_DiskPartition | Select-Object -ExpandProperty DeviceID }
+        "sys_programas_instalados" { Get-WmiObject Win32_Product | Format-Table Name, Version }
+        "sys_rede" { Get-WmiObject Win32_NetworkAdapter | Where-Object {$_.MACAddress} | Format-Table Name, MACAddress }
+        "sys_sistema_operacional" { Get-WmiObject Win32_OperatingSystem | Format-Table Caption, Version, OSArchitecture }
+		"sys_abre_snippet_notepad" { notepad $PROFILE }
+        "sys_abre_snippet_vscode" { code $PROFILE }
+        "sys_atualizar_snippet" { . $PROFILE }
+		"sys_som" { Get-WmiObject Win32_SoundDevice | Format-Table Caption }
+        "sys_usb" { Get-WmiObject Win32_PnPSignedDriver | Format-Table DeviceName }
+        "sys_usuarios" { Get-LocalUser | Format-Table Name, Enabled }
 
 	# ===== EXTENSÕES =====
 	"instal_extens_inic" {
@@ -170,6 +175,7 @@ function z {
             Write-Host "ls -Directory                     # listar somente pastas"
             Write-Host "ls -File                          # listar somente arquivos"
             Write-Host "ls -Recurse                       # lista tudo (subpastas)"
+			Write-Host "Invoke-Item                       # abrir arquivo ou pasta"
             Write-Host "ls C:\Users *documen* -File -Recurse       # pesquisa por arquivos com trecho %documen%"
         }
 
@@ -197,12 +203,13 @@ Register-ArgumentCompleter -CommandName z -ParameterName comando -ScriptBlock {
     param($commandName, $parameterName, $wordToComplete)
 
     $comandos = @(
-    "bateria","bios","bluetooth","buscar_por_nome","comandos_vscode","cpu","desktop","disco_espaco","discoinfo",
-	"discos","help","home","gpu","impressoras","instal_extens_inic","instal_extens_py_sql",
-	"ip","ip_geral","listar","listar_force","listar_so_pastas","listar_so_arquivos",
-	"listar_recursivo","memoria_slots","memorias","modelo_pc","onde_estou?","particoes",
-	"placa_mae","programas_instalados","rede","sistema_operacional","snippet_abrir",
-	"snippet_atualizar","som","usb","usuarios","volta_1_diretorio","volta_2_diretorios"          
+        "comandos_vscode","help","nav_buscar_por_nome","nav_desktop","nav_downloads","nav_home","nav_listar","nav_listar_ocultos",
+		"nav_listar_so_pastas","nav_listar_so_arquivos","nav_listar_recursivo","nav_volta_1_diretorio","nav_volta_2_diretorios",
+		"nav_volta_desktop","nav_volta_home","sys_bateria","sys_bios","sys_bluetooth","sys_cpu","sys_disco_espaco","sys_discoinfo",
+		"sys_discos","sys_gpu","sys_impressoras","sys_ip","sys_ip_geral","sys_memoria_slots","sys_memorias","sys_modelo_pc",
+		"nav_onde_estou?","sys_particoes","sys_placa_mae","sys_programas_instalados","sys_rede","sys_sistema_operacional",
+		"sys_abre_snippet_notepad","sys_abre_snippet_vscode","sys_atualizar_snippet","sys_bateria","sys_bios","sys_bluetooth",
+		"sys_som","sys_usb","sys_usuarios","instal_extens_inic","instal_extens_py_sql"
     )
 
     $comandos | Where-Object { $_ -like "$wordToComplete*" } |
@@ -213,6 +220,9 @@ Register-ArgumentCompleter -CommandName z -ParameterName comando -ScriptBlock {
 
 # MENU COM TAB
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
+# MENSAGEM quando abre o terrminal
+Write-Host "Perfil PowerShell carregado!" -ForegroundColor Green 
 
 function code {
     & "C:\Users\XXX\AppData\Local\Programs\Microsoft VS Code\bin\code.cmd" @args #mudar o caminho do usuário onde está com XXX C:\Users\XXX\AppData\Local\Programs\Microsoft VS Code\bin\code.cmd
